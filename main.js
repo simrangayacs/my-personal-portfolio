c6df222b6aeb451b50ea5a6e795c9e025702757d
Simran kumari <simrangayacs@gmail.com>
2026-08-28 19:41:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+ var typed = new Typed(".text", {
+
+    strings: [
+        "CSE Student",
+        "Software Developer",
+        "Web Developer",
+        "Programmer",
+        "Tech Learner"
+    ],
+
+    typeSpeed: 90,
+
+    backSpeed: 70,
+
+    backDelay: 1200,
+
+    loop: true
+
+});
